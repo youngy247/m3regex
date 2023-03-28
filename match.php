a4e823778ad32143abb5867b834c6cfa0692ece3
Adam Young <adamyoungy678@gmail.com>
2023-03-28 16:45:01 +0100

    created a pregmatch for the resources variable

--- match.php
+++ match.php
@@ -7,3 +7,8 @@ echo '<p>$result is ' . $result . '</p>';
 
 $resources = 'w3schools.com, regex101.com, www.regular-expressions.info';
 
+if (preg_match('/\d/', $resources)) {
+    echo '<p>The resources list contains digits. </p>';
+} else {
+    echo '<p>The resources list NOT containers digits.</p>';
+}
