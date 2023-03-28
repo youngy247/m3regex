d319807c5052fba8dc0518a29136d5bcd96ba359
Adam Young <adamyoungy678@gmail.com>
2023-03-28 16:43:05 +0100

    created new  variable

--- match.php
+++ match.php
@@ -4,3 +4,6 @@ $string = 'Hello world!';
 $result = preg_match('/world/', $string);
 
 echo '<p>$result is ' . $result . '</p>';
+
+$resources = 'w3schools.com, regex101.com, www.regular-expressions.info';
+
